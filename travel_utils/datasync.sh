2f37ce68b745dbf42328794425e386887e013684
andrea-at-915543881499 <andrea.bradsorph@gmail.com>
2025-07-09 20:51:14 +0200

    travel script

---
+++ travel_utils/datasync.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SLIST=tosync.txt
+
+COUNT=0
+
+LISTL=$(grep .  $SLIST|wc -l)
+
+for I in $(cat $SLIST)
+do
+	COUNT=$(expr $COUNT + 1 )
+	echo $COUNT"/"$LISTL" - "$I
+	rsync -a "$I" .
+done
+
